b269b84b270ecfd6fffb8fda8a44bab1562837a6
Katsushi　Furuta（古田　勝士） <154295364+cotaxxxx@users.noreply.github.com>
2026-08-25 12:14:18 +0900

    Add checker package

---
+++ checker/__init__.py
@@ -0,0 +1,5 @@
+"""Prototype numerical kernels for the oblate-spheroid case."""
+
+from .oblate_axis_prototype import b_ob, boundary_energy_ob, g_axis_ob
+
+__all__ = ["b_ob", "boundary_energy_ob", "g_axis_ob"]
