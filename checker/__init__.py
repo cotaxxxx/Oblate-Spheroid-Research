99b7e2ae8cc7b874bf62013d792344f4aaaf8f00
Katsushi　Furuta（古田　勝士） <154295364+cotaxxxx@users.noreply.github.com>
2026-08-26 12:50:24 +0900

    Decouple checker package from prototype evaluator

--- checker/__init__.py
+++ checker/__init__.py
@@ -1,5 +1,1 @@
-"""Prototype numerical kernels for the oblate-spheroid case."""
-
-from .oblate_axis_prototype import b_ob, boundary_energy_ob, g_axis_ob
-
-__all__ = ["b_ob", "boundary_energy_ob", "g_axis_ob"]
+"""Independent checker package for the oblate-spheroid case."""
